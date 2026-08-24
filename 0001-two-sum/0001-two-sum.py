c53ab5fa2b9a42f4546d347362100759682322e6
Oishi-Das <oishidas49@gmail.com>
2026-08-25 04:25:44 +0530

    Time: 3 ms (54.37%), Space: 13.2 MB (36.58%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        for i in range(len(nums)) :
+            curr = nums [i]
+            need = target - curr 
+            if need in seen :
+                return [seen[need] , i ]
+            seen[curr] = i 
