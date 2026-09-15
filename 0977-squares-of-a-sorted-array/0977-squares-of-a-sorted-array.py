b35905c3620b16a90fa4562c1d3a7703f5ff6195
Oishi-Das <oishidas49@gmail.com>
2026-09-15 22:42:02 +0530

    Time: 11 ms (54.18%), Space: 21 MB (72.02%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,49 +1,21 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        
-  
-
         n = len(nums)
+        result = [0] * n
 
-        # Answer array filled with 0s
-        ans = [0] * n
-
-        # Left pointer
         left = 0
-
-        # Right pointer
         right = n - 1
-
-        # Fill answer from the last index
         pos = n - 1
 
-        # Continue until pointers cross
         while left <= right:
-
-            # Square of left element
-            left_square = nums[left] * nums[left]
-
-            # Square of right element
-            right_square = nums[right] * nums[right]
-
-            # Put the larger square at the current last position
-            if left_square > right_square:
-
-                ans[pos] = left_square
-
-                # Move left pointer
+            if abs(nums[left]) > abs(nums[right]):
+                result[pos] = nums[left] * nums[left]
                 left += 1
-
             else:
-
-                ans[pos] = right_square
-
-                # Move right pointer
+                result[pos] = nums[right] * nums[right]
                 right -= 1
 
-            # Move to the previous position in answer array
             pos -= 1
 
-        return ans
-       
-          
+        return result
+        
