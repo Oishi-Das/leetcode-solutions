577757015def001e2f5ceb35a04bf114450fcb5a
Oishi-Das <oishidas49@gmail.com>
2026-09-15 22:43:22 +0530

    Time: 666 ms (31.51%), Space: 22.2 MB (54.67%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,51 +1,34 @@
 class Solution:
-    def threeSum(self, nums):
-
-        # First, sort the array
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
-
-        # This will store all valid triplets
         result = []
 
-        # i chooses the first number
-        for i in range(len(nums)):
-
-            # Skip duplicate first numbers
+        for i in range(len(nums) - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            # Two pointers for finding the other two numbers
             left = i + 1
             right = len(nums) - 1
 
-            # Keep searching while the pointers haven't crossed
             while left < right:
-
-                # Calculate the sum of the three numbers
                 total = nums[i] + nums[left] + nums[right]
 
-                # Sum is too small
-                if total < 0:
-                    left += 1
-
-                # Sum is too large
-                elif total > 0:
-                    right -= 1
-
-                # We found a triplet
-                else:
+                if total == 0:
                     result.append([nums[i], nums[left], nums[right]])
 
-                    # Move both pointers
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
                     left += 1
                     right -= 1
 
-                    # Skip duplicate left values
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-
-                    # Skip duplicate right values
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
 
         return result
+        
